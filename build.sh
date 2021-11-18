0eff2daa92e4126e3a6afc56f22359c0f7ab809c
Kazuki Takashima <takashima.kazuki@ais.cmc.osaka-u.ac.jp>
2021-11-18 18:18:46 +0900

    ビルドスクリプト修正

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 curDir=$(cd $(dirname $0);pwd) 
 sudo docker run -v $curDir:/app --privileged --rm -t -e container=docker doca_v1.11_bluefield_os_ubuntu_20.04-mlnx-5.4  /app/simple_fwd_vnf/src/build_app.sh
+
+scp $curDir/simple_fwd_vnf/src/build/doca_simple_fwd_vnf bluefield:/tmp
