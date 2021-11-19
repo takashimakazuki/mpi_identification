2b13fbf399654c81aacaa51b727733cd21425785
Kazuki Takashima <takashima.kazuki@ais.cmc.osaka-u.ac.jp>
2021-11-19 09:18:47 +0900

    プログラム

---
+++ sample/mpi_send.c
@@ -0,0 +1,33 @@
+#include "mpi.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    // Initialize the MPI environment
+    MPI_Init(&argc, &argv);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    int number;
+    if (world_rank == 0)
+    {
+        number = 0xdeadbeef;
+        MPI_Send(&number, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+    }
+    else if (world_rank == 1)
+    {
+        MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD,
+                 MPI_STATUS_IGNORE);
+        printf("Process 1 received number %d from process 0\n",
+               number);
+    }
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
+}
