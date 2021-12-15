66ebe6033999b467e483d5091b740c65ff7f0c9f
Kazuki Takashima <takashima.kazuki@ais.cmc.osaka-u.ac.jp>
2021-12-15 17:28:13 +0900

    ディレクトリ構造を修正

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 curDir=$(cd $(dirname $0);pwd) 
-sudo docker run -v $curDir:/app --privileged --rm -t -e container=docker doca_v1.11_bluefield_os_ubuntu_20.04-mlnx-5.4  /app/simple_fwd_vnf/src/build_app.sh
+sudo docker run -v $curDir:/app --privileged --rm -t -e container=docker doca_v1.11_bluefield_os_ubuntu_20.04-mlnx-5.4  /app/src/build_app.sh
 
-scp $curDir/simple_fwd_vnf/src/build/doca_simple_fwd_vnf bluefield:/tmp
+scp $curDir/src/build/log_mpi bluefield:/tmp
